a0becf4c377c37a21c5689b90c4aa03281284e6a
Trisnadi Sutrisno <bijirin@Bijirin-MacBook-Air.local>
2026-03-20 18:54:46 +0700

    updated main.py per instruction

--- app/main.py
+++ app/main.py
@@ -1,13 +1,27 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    new_error = {}
+    
+    new_error["line"] = error["line_number"]
+    new_error["column"] = error["column_number"]
+    new_error["message"] = error["text"]
+    new_error["name"] = error["code"]
+    new_error["source"] = "flake8"
+    
+    return new_error
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    new_file = {}
 
+    new_file["errors"] = [format_linter_error(error) for error in errors]
+    new_file["path"] = file_path
+
+    if new_file["errors"] == []:
+        new_file["status"] = "passed"
+    else:
+        new_file["status"] = "failed"
+
+    return new_file
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [format_single_linter_file(file, linter_report[file]) for file in linter_report]
