8af051146e508a9a57db2a2f979a4f457c7b537d
Trisnadi Sutrisno <bijirin@Bijirin-MacBook-Air.local>
2026-03-20 19:28:21 +0700

    still flake8 corrections

--- app/main.py
+++ app/main.py
@@ -1,15 +1,15 @@
 def format_linter_error(error: dict) -> dict:
     return dict(line=error["line_number"],
-        column=error["column_number"], 
-        message=error["text"], 
-        name=error["code"], 
-        source="flake8")
+                column=error["column_number"],
+                message=error["text"],
+                name=error["code"],
+                source="flake8")
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return dict(errors=[format_linter_error(error) for error in errors], 
-        path=file_path, 
-        status="passed" if not errors else "failed")
+    return dict(errors=[format_linter_error(error) for error in errors],
+                path=file_path,
+                status="passed" if not errors else "failed")
 
 
 def format_linter_report(linter_report: dict) -> list:
